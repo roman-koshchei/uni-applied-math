58999dedfdbda6b4a7cf81eefe399c75c9bed4ac
Roman Koshchei <romankoshchei@gmail.com>
2024-09-18 11:36:51 +0300

    lab 1 work

--- src/labs/lab_1.py
+++ src/labs/lab_1.py
@@ -12,6 +12,10 @@ def run():
   problem_3_1_13()
   print()
   problem_5_1_13()
+  print()
+  problem_5_3_13()
+  print()
+  problem_6_2_13()
 
 
 def problem_2_1_13():
@@ -58,15 +62,48 @@ def problem_3_1_13():
   print("x =", x)
 
 def problem_5_1_13():
+  a, b = sp.symbols('a,b')
+  expression = sp.expand((5*a + 2*b) * (-1*a + 3*b)) 
+
+  print("5.1.13 (a)")
+  print(expression)
+
   angle = np.pi / 4
   moduleA = math.sqrt(2)
   moduleB = 3
 
-  a, b = sp.symbols('a,b')
-  expression = sp.expand((5*a + 2*b) * (-1*a + 3*b)) 
-  print(expression)
-  # sp.solvel
+  res = math.sqrt(moduleA**2 - 2 * moduleA * moduleB * math.cos(angle) + moduleB**2)
+  print("5.1.13 (b)")
+  print(res)
 
-  print("5.1.13 (a)")
+def problem_5_3_13():
+  a = np.array([
+    [3, -2, 3],
+    [2, -1, 3],
+    [5, 1, 12]
+  ])
   
+  b = np.array([10, 4, 0])
+
+  x = np.linalg.solve(a, b)
+  print("5.3.13")
+  print(x)
+
+def problem_6_2_13():
+  a = np.array([0, -2, 4])
+  b = np.array([-2, -2, 1])
+  c = np.array([3, -3, 2])
+  d = np.array([3, 3, 4])
+  
+  ab = b - a
+  ac = c - a
+  ad = d - a
+
+  s = np.linalg.norm(np.cross(ab, ac)) / 2
+  v = abs(np.dot(ab, np.cross(ac, ad))) / 6
+
+  print("6.2.13")
+  print("S =", s)
+  print("V =", v)
+
   
